58070631dc4a734c9183633c1e3c06b8797a87ba
pontakornth <pontakorn61@gmail.com>
2021-01-24 18:02:50 +0700

    feat: Create bare bone game component

---
+++ client/src/routes/controller/index.tsx
@@ -0,0 +1,7 @@
+import { h, FunctionalComponent } from "preact";
+
+const Controller: FunctionalComponent = () => {
+    return <div class="controller">This is controller</div>;
+};
+
+export default Controller;
---
+++ client/src/routes/game/index.tsx
@@ -0,0 +1,7 @@
+import { h, FunctionalComponent } from "preact";
+
+const Game: FunctionalComponent = () => {
+    return <div class="game">This is game.</div>;
+};
+
+export default Game;
