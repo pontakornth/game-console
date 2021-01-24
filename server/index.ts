d6d22128ab19bb83b94ede8a1a9d341f183e69bf
pontakornth <pontakorn61@gmail.com>
2021-01-24 19:32:12 +0700

    feat: Make server functional

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,25 @@
-import * as express from 'express';
-import * as socketio from "socket.io";
+import express from 'express';
+import socketio from "socket.io";
 import * as http from "http";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
-const io: socketio.Server  = (socketio as any)(server);
+const io: socketio.Server  = (socketio as any)(server, {
+	cors: {
+		origin: '*',
+	}
+});
 
-app.get('/', (req, res) => {
+app.get('/', (req: express.Request, res: express.Response) => {
 	res.send("Hello, world\n");
 })
 
 io.on("connection", (socket: socketio.Socket) => {
 	console.log("Connected")
 	socket.on("left", () => {
+		console.log("Left")
 		socket.broadcast.emit("left")
 	})
 	socket.on("right", () => {
