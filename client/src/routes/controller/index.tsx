26f629790e4a34fe20b84eada0dc27ed72eeda7e
pontakornth <pontakorn61@gmail.com>
2021-01-24 21:37:07 +0700

    feat: Make it move continous ly

--- client/src/routes/controller/index.tsx
+++ client/src/routes/controller/index.tsx
@@ -1,20 +1,28 @@
 import { h, FunctionalComponent } from "preact";
-import { useEffect } from "preact/hooks";
+import { useEffect, useRef } from "preact/hooks";
 import { io, Socket } from "socket.io-client";
 
 // I don't know if there is a better way to do this but I hope there is
 let socket: Socket;
 
 const Controller: FunctionalComponent = () => {
+    const intervalSet = useRef<NodeJS.Timeout>();
     useEffect(() => {
         socket = io("http://localhost:4000");
     });
     const handleClick = () => {
-        socket.emit("left");
+        intervalSet.current = setInterval(() => {
+            socket.emit("left");
+        }, 500);
+    };
+    const handleUnclick = () => {
+        clearInterval(intervalSet.current);
     };
     return (
         <div class="controller">
-            <button onClick={handleClick}>Left</button>
+            <button onMouseUp={handleUnclick} onMouseDown={handleClick}>
+                Left
+            </button>
             <p>This is controller</p>
         </div>
     );
